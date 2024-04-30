88d6f51ca773f9cbeba71ab54f042bacf9d47c75
lnvictor <victorhpereira26@gmail.com>
2024-04-29 23:58:28 -0300

    feat: setup project with initial files

---
+++ src/main/java/com/br/ccbrec/CcbrecApplication.java
@@ -0,0 +1,13 @@
+package com.br.ccbrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbrecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbrecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ccbrec/CcbrecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ccbrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbrecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
