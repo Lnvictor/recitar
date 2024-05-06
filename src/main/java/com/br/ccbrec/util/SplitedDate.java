26aef8632fa9d826af8767e46a30ea5724ba9631
lnvictor <victorhpereira26@gmail.com>
2024-05-05 23:22:18 -0300

    feat: using new class to represent custom date

---
+++ src/main/java/com/br/ccbrec/util/SplitedDate.java
@@ -0,0 +1,12 @@
+package com.br.ccbrec.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SplitedDate {
+    private String day;
+    private String month;
+    private String year;
+}
