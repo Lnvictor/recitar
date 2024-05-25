fcd29e65c65139abddd7e2fc7112f15ab8d1da2a
lnvictor <victorhpereira26@gmail.com>
2024-05-24 21:21:54 -0300

    feat(10): DTOs

--- src/main/java/com/br/ccbrec/dto/RecitativosCountDTO.java
+++ src/main/java/com/br/ccbrec/dto/RecitativosCountDTO.java
@@ -1,6 +1,8 @@
 package com.br.ccbrec.dto;
 
+import com.br.ccbrec.entities.Recitativos;
 import com.br.ccbrec.entities.RecitativosCount;
+import com.br.ccbrec.entities.YouthCult;
 import com.br.ccbrec.util.DateUtils;
 import com.br.ccbrec.util.SplitedDate;
 import jakarta.validation.constraints.*;
@@ -36,8 +38,9 @@ public class RecitativosCountDTO {
     private int individuals;
 
     public static RecitativosCountDTO fromDTO(RecitativosCount count) {
-        return new RecitativosCountDTO(RecitativosCountDTO.getFormattedDate(count.getDay(), count.getMonth(), count.getYear()),
-                count.getGirls(), count.getBoys(), count.getYoungGirls(), count.getYoungBoys(), count.getIndividuals());
+        return new RecitativosCountDTO(RecitativosCountDTO.getFormattedDate(count.getYouthCult().getDay(),
+                count.getYouthCult().getMonth(), count.getYouthCult().getYear()), count.getGirls(), count.getBoys(),
+                count.getYoungGirls(), count.getYoungBoys(), count.getIndividuals());
     }
 
     public static String getFormattedDate(String day, String month, String year) {
@@ -52,10 +55,12 @@ public class RecitativosCountDTO {
         RecitativosCount entity = new RecitativosCount();
         SplitedDate map = DateUtils.splitRecitativosDate(this.date);
 
-        entity.setYear(map.getYear());
-        entity.setMonth(map.getMonth());
-        entity.setDay(map.getDay());
+        YouthCult youthCult = new YouthCult();
+        youthCult.setYear(map.getYear());
+        youthCult.setMonth(map.getMonth());
+        youthCult.setDay(map.getDay());
 
+        entity.setYouthCult(youthCult);
         entity.setBoys(this.boys);
         entity.setGirls(this.girls);
         entity.setYoungBoys(this.youngBoys);
